4054f7828ebe07ff7989ddb70b1ce60f24dacb93
nguyen trung <nguyenthanhtrung@gmail.com>
2023-07-11 14:49:54 +0700

    init

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        var ss =  StdIn.readString();
+        StdOut.println(ss);
+    }
+
+}
